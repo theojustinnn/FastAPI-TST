6ca9a6e8a9ab1b7aa350ff8d9eaa38ca4c018193
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2022-09-27 21:49:34 +0700

    Add FastAPI Python File

---
+++ api_mahasiswa.py
@@ -0,0 +1,36 @@
+# Nama  : Theodore Justin Lionar
+# NIM   : 18220011
+# Tugas : FastAPI
+
+# Import class
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Create instance
+app = FastAPI()
+
+# Create model
+class Identitas(BaseModel):
+    Name: str
+    NIM: int
+
+# Create data
+data_mahasiswa = {
+    1: {
+        "Nama": "Theodore Justin",
+        "NIM" : 18220011
+    }
+}
+
+# Define path for view data
+@app.get("/view")
+def view_name():
+    return data_mahasiswa
+
+# Define path for add data
+@app.post("/add/{id}")
+def add_name(id: int, identitas: Identitas):
+    if id in data_mahasiswa:
+        return {"Error" : "Mahasiswa Sudah Terdaftar"}
+    data_mahasiswa[id] = {"Nama" : identitas.Name, "NIM" : identitas.NIM}
+    return data_mahasiswa[id]
